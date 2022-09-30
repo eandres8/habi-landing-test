b241883a73ebb1a443dc861aee8265de56859ddf
Andres Vega <elb.andres8@gmail.com>
2022-09-30 10:14:39 -0500

    fix styles Navbar component to be fixed on top

--- src/components/styled/Navbar.ts
+++ src/components/styled/Navbar.ts
@@ -4,10 +4,21 @@ import { THEME } from '../../constants/design';
 
 export const Navbar = styled.nav`
     margin: 0;
-    padding: 15px;
+    padding: 20px 15px;
     display: flex;
+    width: 100%;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    height: 50px;
     justify-content: center;
     box-shadow: ${props => props.theme.shadow};
+    background-color: ${props => props.theme.color.white};
+
+    & > img {
+        width: 50px;
+    }
 `;
 
 Navbar.defaultProps = {
